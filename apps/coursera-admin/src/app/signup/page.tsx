eb9c6c4745cc37241b7179024f290a24babf0110
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-10 19:04:56 +0530

    signup page created for username/password authentication

---
+++ apps/coursera-admin/src/app/signup/page.tsx
@@ -0,0 +1,79 @@
+import { lucia } from "@/auth";
+import { generateId } from "lucia";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { Argon2id } from "oslo/password";
+import { prisma } from "@/lib/prisma";
+
+export default async function Page() {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <form action={signup}>
+        <label htmlFor="username">Username</label>
+        <input name="username" id="username" style={{ color: "black" }} />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          style={{ color: "black" }}
+        />
+        <br />
+        <button type="submit">Continue</button>
+      </form>
+    </>
+  );
+}
+
+async function signup(formData: FormData): Promise<ActionResult> {
+  "use server";
+  const username = formData.get("username");
+  // username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
+  // keep in mind some database (e.g. mysql) are case insensitive
+  if (
+    typeof username !== "string" ||
+    username.length < 3 ||
+    username.length > 31 ||
+    !/^[a-z0-9_-]+$/.test(username)
+  ) {
+    return {
+      error: "Invalid username",
+    };
+  }
+  const password = formData.get("password");
+  if (
+    typeof password !== "string" ||
+    password.length < 6 ||
+    password.length > 255
+  ) {
+    return {
+      error: "Invalid password",
+    };
+  }
+
+  const hashedPassword = await new Argon2id().hash(password);
+  const adminId = generateId(15);
+
+  await prisma.admin.create({
+    data: {
+      id: adminId,
+      adminname: username,
+      hashed_password: hashedPassword,
+    },
+  });
+
+  const session = await lucia.createSession(adminId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+  return redirect("/");
+}
+
+interface ActionResult {
+  error: string;
+}
