241119bef2a2ea2f803550801a5022949a78605f
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-10 22:37:24 +0530

    admin route created for creating a new course

---
+++ apps/coursera-admin/src/app/api/createCourse/route.ts
@@ -0,0 +1,40 @@
+import { CourseAttributes } from "@/app/trial/page";
+import { prisma } from "@/lib/prisma";
+import { Session, User } from "lucia"
+
+interface SessionAttributes {
+    session: Session,
+    user: User
+}
+
+export async function POST(req: Request): Promise<Response> {
+    try {
+        const sessionDataHeader = req.headers.get('session-data')
+        if (!sessionDataHeader) {
+            return Response.json({ message: "sessionDataHeader not found" });
+        }
+        const { session, user }: SessionAttributes = JSON.parse(sessionDataHeader);
+        const adminId = session.userId  //this data comes directly from the db. hence you do not need a prisma call to test if it exists in db.
+
+        const body: CourseAttributes = await req.json();//giving it a type is important or else the prisma call below won't recognize the data while using a spred operator.
+        const course = body
+        const newCourse = await prisma.course.create({
+            data: {
+                ...course,
+                admin: {
+                    connect: {
+                        id: adminId
+                    }
+                }
+            },
+        })
+
+        return Response.json({ message: "course created successfully" }, { status: 200 })
+        //you shouldn't return a message if a course with the same details alreay exists, you cannnot stop people from creating similar courses.
+        //ofcourse these courses would have one differing attribute, the id.
+
+    } catch (error) {
+        console.error(error)
+        return Response.json({ message: "internal server error" }, { status: 500 })
+    }
+}
