3464fec245ad6da09419dde53d3633acc31eb7ee
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-10 09:03:48 +0530

    route created to get user details

---
+++ apps/coursera/src/app/api/me/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma"
+import { Session, User } from "lucia"
+
+export async function GET(req: Request): Promise<Response> {
+    try {
+        const sessionDataHeader = req.headers.get('session-data')
+        if (!sessionDataHeader) {
+            return Response.json({ message: "sessionDataHeader not found" }, { status: 400 })
+        }
+        const { session, user }: { session: Session, user: User } = JSON.parse(sessionDataHeader)
+        const userId = session.userId
+        const me = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                id: true,
+                avatar: true,
+                username: true,
+                email: true,
+                courses: true
+            }
+        })
+        return Response.json(me, { status: 200 })
+    } catch (error) {
+        console.error(error)
+        return Response.json({ error: "internal server error" }, { status: 500 })
+    }
+}
