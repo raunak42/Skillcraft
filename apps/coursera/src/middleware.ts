68f1df6e103ca760a1de902568f039e66d712350
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-10 09:29:45 +0530

    middleware changed

--- apps/coursera/src/middleware.ts
+++ apps/coursera/src/middleware.ts
@@ -7,9 +7,12 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { validateRequest } from "./auth";
 
+const unprotectedRoutes = ["/api/getCourses"] //for ssr //could have gone with the matcher approach for ssr routes but mathcer blocks the entire middleware for the path that is not included, that means session verification would have stopped but on top of that csrf prevention would also have stopped.
+
 export async function middleware(request: NextRequest): Promise<NextResponse> {
 	const { session, user } = await validateRequest();
-	if (!session) {
+	const unprotectedRoute = unprotectedRoutes.find((t) => t === request.nextUrl.pathname)
+	if (!unprotectedRoute && !session) {
 		return NextResponse.json({ message: "Sign in first." }, {
 			status: 403
 		})
@@ -31,6 +34,8 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 }
 
 export const config = {
-	matcher: ["/api/:path*"] //spare the paths that are to be used for ssr, these paths don't require authentication, they are for seo
+	matcher: [
+		"/api/:path*",
+	]
 }
 //middleware here, works for every path including frontend paths if not configuerd with a matcher
