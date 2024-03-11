5515b5a2067f2b095251c4d300aa929a3f9e127a
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-11 20:39:04 +0530

    minor changes

--- apps/coursera-admin/src/app/api/createCourse/route.ts
+++ apps/coursera-admin/src/app/api/createCourse/route.ts
@@ -2,7 +2,7 @@ import { CourseAttributes } from "@/app/trial/page";
 import { prisma } from "@/lib/prisma";
 import { Session, User } from "lucia"
 
-interface SessionAttributes {
+export interface SessionAttributes {
     session: Session,
     user: User
 }
@@ -11,13 +11,14 @@ export async function POST(req: Request): Promise<Response> {
     try {
         const sessionDataHeader = req.headers.get('session-data')
         if (!sessionDataHeader) {
-            return Response.json({ message: "sessionDataHeader not found" });
+            return Response.json({ message: "sessionDataHeader not found" }, { status: 500 });//500 internal server error because middleware not working
         }
         const { session, user }: SessionAttributes = JSON.parse(sessionDataHeader);
         const adminId = session.userId  //this data comes directly from the db. hence you do not need a prisma call to test if it exists in db.
 
-        const body: CourseAttributes = await req.json();//giving it a type is important or else the prisma call below won't recognize the data while using a spred operator.
-        const course = body
+        const body = await req.json();//giving it a type is important or else the prisma call below won't recognize the data while using a spred operator.
+        const course: CourseAttributes = body
+        
         const newCourse = await prisma.course.create({
             data: {
                 ...course,
@@ -27,9 +28,12 @@ export async function POST(req: Request): Promise<Response> {
                     }
                 }
             },
-        })
+        });
+        if (!newCourse) {
+            return Response.json({ message: "couldn't create course, database didn't respond" })
+        }
 
-        return Response.json({ message: "course created successfully" }, { status: 200 })
+        return Response.json({ message: "course created successfully", newCourse }, { status: 200 })
         //you shouldn't return a message if a course with the same details alreay exists, you cannnot stop people from creating similar courses.
         //ofcourse these courses would have one differing attribute, the id.
 
