ca3b458264e171a432aabaa5be0106ce206a08c7
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-11 21:58:57 +0530

    admin route created to get createdCourses

---
+++ apps/coursera-admin/src/app/api/myCourses/route.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma";
+import { SessionAttributes } from "../createCourse/route";
+
+export async function GET(req: Request): Promise<Response> {
+    try {
+        const sessionDataHeader = req.headers.get('session-data');
+        if (!sessionDataHeader) {
+            return Response.json({ message: "sessionDataHeader not found" }, { status: 500 })
+        };
+        const { session, user }: SessionAttributes = JSON.parse(sessionDataHeader);
+        const adminId = session.userId;
+
+        const admininDb = await prisma.admin.findUnique({
+            where: {
+                id: adminId
+            },
+            include: {
+                createdCourses: true
+            }
+        });
+        if (!admininDb) {
+            return Response.json({ message: "admin doesn't exist" }, { status: 404 })
+        }
+        const myCourses = admininDb.createdCourses;
+        return Response.json(myCourses, { status: 200 })
+    } catch (error) {
+        console.error(error);
+        return Response.json({ message: "internal server error" }, { status: 500 })
+    }
+}
