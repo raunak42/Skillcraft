a32b0d0e1169394f8788ba2794dc3d1436d16b41
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-17 00:33:04 +0530

    both apps configured for accepting the helpers package

--- apps/coursera-admin/next.config.mjs
+++ apps/coursera-admin/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["ui", "db", "store", "auth", "zod-validation", "types"],
+  transpilePackages: [
+    "ui",
+    "db",
+    "store",
+    "auth",
+    "zod-validation",
+    "types",
+    "helpers",
+  ],
   webpack: (config) => {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
