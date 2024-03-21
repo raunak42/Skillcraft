95fddd84314016f594de5181f01db9f8ca0fb54e
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-21 16:14:45 +0530

    minor changes

--- packages/helpers/src/functions/validateBody.ts
+++ packages/helpers/src/functions/validateBody.ts
@@ -2,8 +2,8 @@ import { CourseAttributes } from "types";
 import { courseInput } from "zod-validation";
 
 export async function validateBody(req: Request): Promise<CourseAttributes> {
-    const body = await req.json();
-    const course: CourseAttributes = body;
+    const body: CourseAttributes = await req.json();
+    const course = body;
     const validatedCourse = courseInput.parse(course);//This variable shpuld have a type or else the prisma call below won't recognize the data while using a spread operator.
 
     return validatedCourse;
