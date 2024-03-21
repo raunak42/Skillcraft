efb5b63804a5f1cf400da4969176cdb45ce480a5
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-21 16:03:08 +0530

    not found page created

---
+++ apps/coursera-admin/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (<div>
+        <h1>404 | Not Found</h1>
+        <p>The page you're looking for deosn't exist.</p>
+    </div> );
+};
+
+export default NotFound;
