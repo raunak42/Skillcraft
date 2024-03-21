7daa90bf868d382300da720067725a179780d6b5
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-21 16:02:34 +0530

    rudimentary navbar created

--- apps/coursera-admin/src/app/layout.tsx
+++ apps/coursera-admin/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>
+          <Navbar />
+        </h1>
+        {children}
+        <h1>
+          <Footer />
+        </h1>
+      </body>
     </html>
   );
 }
