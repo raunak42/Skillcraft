4764874834656b6e0cd426da756f28c43d87035f
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-21 16:09:16 +0530

    trying out some components

---
+++ apps/coursera-admin/src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import Links from "./links/Links";
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+      <div>
+        <Links/>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
