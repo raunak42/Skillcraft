8c24ccec767bea7550c3c1ad940ee176349bda94
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-21 16:00:40 +0530

    middleware configured to not run for /api/signup

--- apps/coursera-admin/src/middleware.ts
+++ apps/coursera-admin/src/middleware.ts
@@ -8,22 +8,30 @@ import { NextRequest } from "next/server";
 import { validateRequest } from "./auth";
 
 const unprotectedRoutes = ["/api/getCourses"] //for ssr //could have gone with the matcher approach for ssr routes but mathcer blocks the entire middleware for the path that is not included, that means session verification would have stopped but on top of that csrf prevention would also have stopped.
+const noMiddlewareRoutes = ["/api/signup"]
 
-export async function middleware(request: NextRequest): Promise<NextResponse> {
+export async function middleware(request: NextRequest): Promise<NextResponse | undefined> {
 
     const { session, user } = await validateRequest();
     const unprotectedRoute = unprotectedRoutes.find((t) => t === request.nextUrl.pathname)
+    const noMiddlewareRoute = noMiddlewareRoutes.find((t) => t === request.nextUrl.pathname)
 
+    if (noMiddlewareRoute) {
+        return;
+    }
+    console.log(0)
     if (!unprotectedRoute && !session) {
         return NextResponse.json({ message: "Sign in first." }, {
             status: 403
         })
     }
+    console.log(1)
     const response = NextResponse.next();
     response.headers.set("session-data", JSON.stringify({ session, user }));
     if (request.method === "GET") {
         return response;
     }
+    console.log(2)
     const originHeader = request.headers.get("Origin");
     const hostHeader = request.headers.get("Host"); // NOTE: You may need to use `X-Forwarded-Host` instead when using reverse proxy setups or load balancers
     if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
@@ -31,13 +39,14 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
             status: 403
         });
     }
+    console.log(3)
     return response;
 
 }
 
 export const config = {
     matcher: [
-        "/api/:path*",
+        '/api/:path*',
     ]
 }
 //middleware here, works for every path including frontend paths if not configuerd with a matcher
