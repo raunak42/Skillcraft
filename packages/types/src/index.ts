b12bba6de12c68fd171da46215a67e7b31ce7dfb
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-24 23:33:33 +0530

    new types created with prisma's generated types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,19 +1,48 @@
-import { z } from "zod";
-import { courseInput, userInput, adminInput } from "zod-validation";
+import { Prisma } from '@prisma/client';
 import { Session, User } from "lucia"
-
-export type CourseAttributes = z.infer<typeof courseInput>;
 
 export interface SessionAttributes {
     session: Session,
     user: User
 }
 
-export interface CourseIdParams {
-    params: {
-        courseId: string
-    }
+// User types
+export type PrismaUserInput = Prisma.UserCreateInput;
+export type PrismaUserOutput<T extends Prisma.UserDefaultArgs> = Prisma.UserGetPayload<T>;
+
+// userSession types
+export type PrismaUserSessionInput = Prisma.userSessionCreateInput;
+export type PrismaUserSessionOutput<T extends Prisma.userSessionDefaultArgs> = Prisma.userSessionGetPayload<T>;
+
+// UserOAuthAccount types
+export type PrismaUserOAuthAccountInput = Prisma.UserOAuthAccountCreateInput;
+export type PrismaUserOAuthAccountOutput<T extends Prisma.UserOAuthAccountDefaultArgs> = Prisma.UserOAuthAccountGetPayload<T>;
+
+// Admin types
+export type PrismaAdminInput = Prisma.AdminCreateInput;
+export type PrismaAdminOutput<T extends Prisma.AdminDefaultArgs> = Prisma.AdminGetPayload<T>;
+
+// AdminSession types
+export type PrismaAdminSessionInput = Prisma.AdminSessionCreateInput;
+export type PrismaAdminSessionOutput<T extends Prisma.AdminSessionDefaultArgs> = Prisma.AdminSessionGetPayload<T>;
+
+// AdminOAuthAccount types
+export type PrismaAdminOAuthAccountInput = Prisma.AdminOAuthAccountCreateInput;
+export type PrismaAdminOAuthAccountOutput<T extends Prisma.AdminOAuthAccountDefaultArgs> = Prisma.AdminOAuthAccountGetPayload<T>;
+
+// Course types
+export type PrismaCourseInput = Prisma.CourseCreateInput;
+export type PrismaCourseOutput<T extends Prisma.CourseDefaultArgs> = Prisma.CourseGetPayload<T>;
+
+//ApiResponse schema for infering same types on both backend and frontend.
+export interface ApiResponseAttributes {
+    data?: {
+        admin?: Partial<PrismaAdminOutput<{ select: {}, include: {} }>>,
+        user?: Partial<PrismaUserOutput<{ select: {}, include: {} }>>,
+        course?: Partial<PrismaCourseOutput<{ select: {}, include: {} }>>,
+        courses?: Partial<PrismaCourseOutput<{ select: {}, include: {} }>>[]
+    },
+    message?: string | string[],
+    error?: string | unknown,
 }
 
-export type UserInputAttributes = z.infer<typeof userInput>
-export type AdminInputAttributes = z.infer<typeof adminInput>
