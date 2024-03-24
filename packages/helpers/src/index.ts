10165c26f8bafca077370a145db0b3ef48c83a49
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-24 23:33:49 +0530

    minor changes

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./functions/getSessionDataFromMiddleware";
-export * from "./functions/validateBody";
+export * from "./functions/validateCourseBody";
 export * from "./functions/randomWord";
+export * from "./functions/apiResponse"
+export * from "./functions/handleApiError"
