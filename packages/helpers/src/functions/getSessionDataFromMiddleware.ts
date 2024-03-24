4c20f1ccd50b3992bc0b5bcdf4674cdc2e41ab2b
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-24 23:07:22 +0530

    function modified

--- packages/helpers/src/functions/getSessionDataFromMiddleware.ts
+++ packages/helpers/src/functions/getSessionDataFromMiddleware.ts
@@ -1,11 +1,10 @@
 import { SessionAttributes } from "types";
 
-export function getSessionDataFromMiddleware(req: Request): Response | SessionAttributes {
+export function getSessionDataFromMiddleware(req: Request): SessionAttributes | null {
     const sessionDataHeader = req.headers.get('session-data')
     if (!sessionDataHeader) {
-        return Response.json({ message: "sessionDataHeader not found" }, { status: 500 });//500 internal server error because middleware not working
+        return null;
     }
     const { session, user }: SessionAttributes = JSON.parse(sessionDataHeader);
-
     return { session, user };
 }
