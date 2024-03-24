b76d680a8c9ba17301aba83c675ac077169e74d4
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-03-24 23:34:33 +0530

    some logic moved to be handled at the api

--- apps/coursera-admin/src/app/login/page.tsx
+++ apps/coursera-admin/src/app/login/page.tsx
@@ -1,16 +1,14 @@
 import { lucia } from "@/auth";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { Argon2id } from "oslo/password";
-import { prisma } from "@/lib/prisma";
+import { Session } from "lucia";
+import { ApiResponseAttributes } from "types";
 
 export default async function Page() {
   return (
     <div>
       <h1>Sign in</h1>
       <a href="/login/github">Sign in with GitHub</a>
-      <br />
-      <a href="/login/google">Sign in with Google</a>
+      <br />u<a href="/login/google">Sign in with Google</a>
       <br />
       <h1>Sign in with username and password</h1>
       <form action={login}>
@@ -30,64 +28,48 @@ export default async function Page() {
     </div>
   );
 }
-async function login(formData: FormData): Promise<ActionResult> {
+async function login(formData: FormData) {
   "use server";
-  const username = formData.get("username");
-  const password = formData.get("password");
+  const username = formData.get("username") as string;
+  const password = formData.get("password") as string;
 
-  if (
-    typeof username !== "string" ||
-    username.length < 3 ||
-    username.length > 31 ||
-    !/^[a-z0-9_-]+$/.test(username)
-  ) {
-    return {
-      error: "Invalid username",
-    };
-  }
-  if (
-    typeof password !== "string" ||
-    password.length < 6 ||
-    password.length > 255
-  ) {
-    return {
-      error: "Invalid password",
-    };
-  }
-
-  const existingUser = await prisma.admin.findUnique({
-    where: {
-      adminname: username,
-    },
+  const variable = await fetch("http://localhost:3001/api/login", {
+    method: "POST",
+    body: JSON.stringify({ username, password }),
   });
 
-  const hashed_password = await new Argon2id().hash(password);
+  const response: ApiResponseAttributes = await variable.json();
 
-  if (!existingUser || !hashed_password) {
-    return {
-      error: "Incorrect username or password",
-    };
+  const { data, message, error } = response;
+
+  if (!data || !data.admin) {
+    return;
+  }
+  const admin = data.admin;
+  const adminId = admin.id as string;
+  const session = await lucia.createSession(adminId, {});
+  startSession(session);
+
+  if (error) {
+    ("use server");
+    console.error(error);
+  }
+  if (message) {
+    //
   }
 
-  const validPassword = await new Argon2id().verify(
-    existingUser.hashed_password,
-    password
-  );
-  if (!validPassword) {
-    return {
-      error: "Incorrect username or password",
-    };
-  }
+  // return redirect("/");
+  // return data;
+}
 
-  const session = await lucia.createSession(existingUser.id, {});
+const startSession = (session: Session) => {
   const sessionCookie = lucia.createSessionCookie(session.id);
   cookies().set(
     sessionCookie.name,
     sessionCookie.value,
     sessionCookie.attributes
   );
-  return redirect("/");
-}
+};
 
 interface ActionResult {
   error: string;
