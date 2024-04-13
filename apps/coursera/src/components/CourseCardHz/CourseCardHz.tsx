b5e6fa7105599a9d7f7cb4d702a180ea9ae588de
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-13 11:17:53 +0530

    horizontal coursecard component created

---
+++ apps/coursera/src/components/CourseCardHz/CourseCardHz.tsx
@@ -0,0 +1,17 @@
+interface CourseCardHzProps {
+  imageLink: string;
+  title: string;
+}
+
+export const CourseCardHz: React.FC<CourseCardHzProps> = ({
+  imageLink,
+  title,
+}) => {
+  return (
+    <div className="relative border rounded-2xl overflow-hidden w-[97%] hover:w-full  transition-all duration-300 ease-in-out mx-auto">
+  <img className="w-full h-[116px] object-cover" src={imageLink} />
+  <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent"></div>
+  <h2 className="absolute bottom-2 left-2 text-white font-bold text-xl">{title}</h2>
+</div>
+  );
+};
