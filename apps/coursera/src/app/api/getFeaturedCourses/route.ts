6830fa0293a0fd4582f9bec4365f7e150abbfbed
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-14 14:29:22 +0530

    search cases optimized

--- apps/coursera/src/app/api/getFeaturedCourses/route.ts
+++ apps/coursera/src/app/api/getFeaturedCourses/route.ts
@@ -1,18 +1,32 @@
 import { prisma } from "@/lib/prisma";
+import { Prisma } from "db";
 import { apiResponse, handleApiError } from "helpers";
 
 export async function GET(req: Request): Promise<Response> {
-    const categories = ['Physics', 'Computer Science', 'Art', 'Health', 'Finance', 'Psychology', 'Writing', 'History']
-
+    const categories = ['Computer Science','Physics', 'Art','Writing','Health', 'Music']
     try {
         //this will return 'n' courses arrays, where n = length of categories-array above.
         const promises = categories.map(async (category) => {
+            const case1 = ` ${category} `
+            const case2 = `${category},`
+            const case3 = ` ${category}`
+            const case4 = ` ${category},`
+
             return await prisma.course.findMany({
                 where: {
                     OR: [
-                        { title: { contains: category, mode: "insensitive" } },
-                        { description: { contains: category, mode: "insensitive" } },
-                        { category: { contains: category, mode: "insensitive" } },
+                        { title: { contains: case1, mode: "insensitive" } },
+                        { title: { contains: case2, mode: "insensitive" } },
+                        { title: { contains: case3, mode: "insensitive" } },
+                        { title: { contains: case4, mode: "insensitive" } },
+                        { description: { contains: case1, mode: "insensitive" } },
+                        { description: { contains: case2, mode: "insensitive" } },
+                        { description: { contains: case3, mode: "insensitive" } },
+                        { description: { contains: case4, mode: "insensitive" } },
+                        { category: { contains: case1, mode: "insensitive" } },
+                        { category: { contains: case2, mode: "insensitive" } },
+                        { category: { contains: case3, mode: "insensitive" } },
+                        { category: { contains: case4, mode: "insensitive" } }
                     ],
                 },
                 take: 18,
