4b085259ef1311a493421e7a7f653fac486303ba
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-14 14:28:55 +0530

    script for injecting data into db

--- apps/coursera/src/app/api/getCourses/route.ts
+++ apps/coursera/src/app/api/getCourses/route.ts
@@ -3,18 +3,49 @@ import { prisma } from "@/lib/prisma";
 //for ssr use
 export async function GET(req: Request): Promise<Response> {
     try {
-        const data = await prisma.course.findMany({
-            select: {
-                id: true,
-                title: true,
-                description: true,
-                imageLink: true,
-                price: true,
-                published: true,
-                adminId: false
-            }
-        });
-        return Response.json(data, { status: 200 })
+        const courses = await prisma.course.findMany({});
+
+        // const totalCourses = physicsCourses.concat(allCourses)
+
+        // const injectData = () => {
+        //     totalCourses.map(async (course) => {
+        //         await prisma.course.create({
+        //             data: {
+        //                 title: course.title as string,
+        //                 description: course.description as string,
+        //                 imageLink: course.imageLink as string,
+        //                 price: course.price as number,
+        //                 category: course.category as string,
+        //                 published: course.published as boolean,
+        //             },
+        //         });
+        //     });
+        // };
+
+        // for (var i = 0; i < 4; i++) {
+        //     injectData();
+        // }
+
+        // const injectData2 = () => {
+        //     courses.map(async (course) => {
+        //         await prisma.course.updateMany({
+        //             where: {
+        //                 title: "Interior Design Basics"
+        //             },
+        //             data: {
+        //                 // title: "Learn the principles of creating beautiful and functional living spaces.",
+        //                 // description: "Towards the grand unification of gravity and quantum mechanics.",
+        //                 // imageLink: "https://www.quantumdiaries.org/wp-content/uploads/2014/03/2000px-Standard_Model_of_Elementary_Particles.svg_.jpg",
+        //                 // price: 99,
+        //                 category: 'design, interior design, art',
+        //                 // published: false
+        //             },
+        //         });
+        //     });
+        // };
+
+        // injectData2()
+        return Response.json(courses, { status: 200 })
     } catch (error) {
         console.error(error)
         return new Response("internal server error", { status: 500 })
