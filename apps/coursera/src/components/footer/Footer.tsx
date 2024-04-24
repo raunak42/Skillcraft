1eb36fb9b1c3bb242bfe8908030bb4a3c8c8fb23
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-24 13:24:26 +0530

    links added to footer images

--- apps/coursera/src/components/footer/Footer.tsx
+++ apps/coursera/src/components/footer/Footer.tsx
@@ -1,19 +1,27 @@
 const Footer = () => {
   return (
-    <div className=" w-full h-[300px] mt-auto border bg-red-50 flex flex-row">
+    <div className=" w-full h-full bg-gray-300 flex flex-row py-10">
       <div className="w-full flex flex-col items-center">
-        <h1 className="text-center text-4xl font-bold text-[#EA3A36] mt-2">
-          Find me on:
+        <h1 className="text-center text-4xl font-bold text-[#000000]">
+          Find me on
         </h1>
-        <div className="flex flex-row justify-center space-x-24 pt-6">
-          <img src="github.svg" className="size-10 hover:cursor-pointer" />
-          <img src="twitter.svg" className="size-10 hover:cursor-pointer" />
-          <img src="gmail.svg" className="size-10 hover:cursor-pointer" />
-          <img src="reddit.svg" className="size-10 hover:cursor-pointer" />
+        <div className="flex flex-row justify-center space-x-12 xl:space-x-32 pt-6">
+          <a href="https://github.com/raunak42">
+            <img src="github.svg" className="size-10 hover:cursor-pointer" />
+          </a>
+          <a href="https://twitter.com/raunak_42">
+            <img src="twitter.svg" className="size-10 hover:cursor-pointer" />
+          </a>
+          <a href="mailto:raunaklanjewar42@gmail.com">
+            <img src="gmail.svg" className="size-10 hover:cursor-pointer" />
+          </a>
+          <a href="https://www.reddit.com/user/RaunakA_/">
+            <img src="reddit.svg" className="size-10 hover:cursor-pointer" />
+          </a>
         </div>
         <div className="flex flex-col items-center mt-20 space-y-2">
           <img src="skillcraftLogo.svg" className="" width={200}></img>
-          <div className="text-[#EA3A36] font-semibold flex flex-col items-center ">
+          <div className="text-[#000000] font-semibold flex flex-col items-center ">
             <h1 className="">Copyright ©2024</h1>
             <div className="flex flex-row space-x-1 ">
               <h1>Created by</h1>
