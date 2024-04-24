24374f8f8d9ca9daf8f4c770b5b2bcb4fbe68421
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-24 13:29:56 +0530

    sidebar changed, added a cross

--- apps/coursera/src/components/Sidebar/Sidebar.tsx
+++ apps/coursera/src/components/Sidebar/Sidebar.tsx
@@ -1,29 +1,28 @@
 "use client";
-import { useState } from "react";
+
+import { useRecoilState } from "recoil";
+import { sideBarOpenState } from "state-store";
 
 export const Sidebar = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const handleFirstButtonClick = () => {
-    setIsExpanded(!isExpanded);
-  };
+  const [sideBarOpen, setSideBarOpen] = useRecoilState(sideBarOpenState);
 
   return (
-    <div
-      className={`fixed left-0 h-full bg-[#ffffff] mt-[70px] z-10 border-[2px] border-r-[#ED2647] shadow-sm transition-all duration-300 ease-liner ${
-        isExpanded ? "w-56" : "w-16"
-      }`}
-    >
-      <button
-        className="w-full hover:bg-gray-200 h-16 flex flex-col justify-center items-center transition-all"
-        onClick={handleFirstButtonClick}
+    <div>
+      <div
+        className={` transition-all duration-300 ease-in-out ${
+          sideBarOpen ? "md:w-[270px] w-[480px]" : "w-0"
+        }`}
       >
-        {isExpanded ? (
-          <img src="arrowBarLeft.svg" className="size-7 ml-auto mr-2" />
-        ) : (
-          <img src="arrowBarRight.svg" className="size-7" />
-        )}
-      </button>
+        <div className="flex flex-row items-center justify-end">
+          <img
+            onClick={() => {
+              setSideBarOpen(false);
+            }}
+            src="cross.svg"
+            className="size-12 hover:cursor-pointer hover:bg-gray-300 rounded-full p-2"
+          ></img>
+        </div>
+      </div>
     </div>
   );
 };
