506e166ac8c153ddd1d0e2a984f9c2e945c0d092
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-24 13:38:10 +0530

    new scripts for injecting data into db

--- apps/coursera/src/app/api/getCourses/route.ts
+++ apps/coursera/src/app/api/getCourses/route.ts
@@ -1,50 +1,53 @@
 import { prisma } from "@/lib/prisma";
+// import fs from "fs"
+// import path from "path";
+// import { courses } from "./courses"
 
 //for ssr use
 export async function GET(req: Request): Promise<Response> {
     try {
         const courses = await prisma.course.findMany({});
 
-        // const totalCourses = physicsCourses.concat(allCourses)
 
         // const injectData = () => {
-        //     totalCourses.map(async (course) => {
-        //         await prisma.course.create({
+        //     courses.map(async (course) => {
+        //         await prisma.course.updateMany({
         //             data: {
-        //                 title: course.title as string,
-        //                 description: course.description as string,
-        //                 imageLink: course.imageLink as string,
-        //                 price: course.price as number,
-        //                 category: course.category as string,
-        //                 published: course.published as boolean,
-        //             },
+        //                 category: course.category
+        //             }
         //         });
         //     });
         // };
+
+        // const injectData = async () => {
+        //     for (let i = 0; i < courses.length; i++) {
+        //         await prisma.course.update({
+        //             where: {
+        //                 id:courses[i].id
+        //             },
+        //             data:{
+        //                 category:courses[i].category
+        //             }
+        //         })
+        //     }
+        // }
+        // injectData();
 
         // for (var i = 0; i < 4; i++) {
         //     injectData();
         // }
 
-        // const injectData2 = () => {
-        //     courses.map(async (course) => {
-        //         await prisma.course.updateMany({
-        //             where: {
-        //                 title: "Interior Design Basics"
-        //             },
-        //             data: {
-        //                 // title: "Learn the principles of creating beautiful and functional living spaces.",
-        //                 // description: "Towards the grand unification of gravity and quantum mechanics.",
-        //                 // imageLink: "https://www.quantumdiaries.org/wp-content/uploads/2014/03/2000px-Standard_Model_of_Elementary_Particles.svg_.jpg",
-        //                 // price: 99,
-        //                 category: 'design, interior design, art',
-        //                 // published: false
-        //             },
-        //         });
-        //     });
-        // };
-
-        // injectData2()
+        // const injectData = async()=>{
+        //     await prisma.course.updateMany({
+        //         where:{
+        //             title:"Standard Model of Partice Physics"
+        //         },
+        //         data:{
+        //             title:"Standard Model of Particle Physics"
+        //         }
+        //     })
+        // }
+        // injectData();
         return Response.json(courses, { status: 200 })
     } catch (error) {
         console.error(error)
