d8ee40a67e3902fe4819b3e693268a6e47d237f1
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-24 13:28:29 +0530

    made the searchbar work

--- apps/coursera/src/components/Searchbar/Searchbar.tsx
+++ apps/coursera/src/components/Searchbar/Searchbar.tsx
@@ -1,26 +1,60 @@
 "use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 interface SearchbarProps {}
 
-function onHitEnter() {}
-
 export const Searchbar: React.FC<SearchbarProps> = () => {
+  const [query, setQuery] = useState<string>("");
+  const firstPage = 1
+  const searchParams = useSearchParams();
+  const persistingText = searchParams.get("q");
+  useEffect(() => {
+    setQuery(persistingText as string);
+  }, []);
+
+  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      if (query.trim().length !== 0 && query !== null && query !== "") {
+        window.location.assign(
+          `/search?q=${encodeURIComponent(query)}&p=${firstPage}`
+        );
+      }
+    }
+  };
+
+  const handleCLick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (query.trim().length !== 0 && query !== null && query !== "") {
+      window.location.assign(
+        `/search?q=${encodeURIComponent(query)}&p=${firstPage}`
+      );
+    }
+  };
+
   return (
-    <div className="border shadow-sm rounded-full w-[600px] flex items-center h-10 bg-gray-200">
-      <div className="px-2">
-        <img src="search.svg" className="size-6"></img>
-      </div>
-      <div className="px-1">
-        <input
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              onHitEnter();
-            }
-          }}
-          placeholder="Search..."
-          type="text"
-          className="bg-inherit w-[540px] focus:outline-none"
-        ></input>
-      </div>
+    <div className="border-[1.5px] border-black shadow-sm rounded-full w-full md:w-[90%] lg:w-[280px] xl:w-[520px] flex items-center h-10 bg-gray-100 pl-4 flex-row gap-3 ">
+      <img src="search.svg" className="size-5 "></img>
+      <input
+        // autoFocus={true}
+        defaultValue={query as string}
+        onChange={(event) => {
+          setQuery(event.currentTarget.value);
+        }}
+        onKeyDown={handleSearch}
+        onFocus={(event) => {
+          setQuery(event.currentTarget.value);
+        }}
+        placeholder="Search..."
+        type="text"
+        className="bg-inherit focus:outline-none w-full"
+      ></input>
+      <button
+        onClick={handleCLick}
+        className="bg-gray-300 hover:bg-gray-200/95 h-full w-[30%] xl:w-[20%] rounded-r-full flex flex-row items-center justify-center"
+      >
+        <img src="search.svg" className="size-6 "></img>
+      </button>
     </div>
   );
 };
