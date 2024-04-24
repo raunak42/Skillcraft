ba23c80d88f0c4fe79a598f8c0caaaed2f8f8cbc
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-24 13:37:17 +0530

    new constants added

--- apps/coursera/src/lib/constants.ts
+++ apps/coursera/src/lib/constants.ts
@@ -1,1 +1,18 @@
 export const BASE_URL_DEV = "http://localhost:3000"
+
+export const SIGNUP_SUCCESS_MESSAGE = "Signed up successfully.";
+export const USERNAME_TAKEN_MESSAGE = "Username taken."
+
+export const INVALID_USRNM_PSWRD_MESSAGE = "Invalid username or password."
+export const LOGIN_SUCCESS_MESSAGE = "Successfully logged in."
+
+export const UNKNOWN_ERR_MESSAGE = "An unknown error occurred."
+
+export const SESSION_HEADER_MISSING_MESSAGE = "sessionDataHeader not found."
+export const COURSE_CREATE_SUCCESS_MESSAGE = "Course created successfully."
+export const COURSE_DELETE_SUCCESS_MESSAGE = "Course deleted successfully."
+export const COURSE_UPDATE_SUCCESS_MESSAGE = "Course updated successfully."
+
+export const PERMISSION_DENIED_MESSAGE = "Permission denied."
+export const ADMIN_NOT_FOUND_MESSAGE = "Admin not found."
+
