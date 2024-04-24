f06763b9562dc770fbf27962be8e0beeea159e03
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-24 13:17:29 +0530

    toggle button for search page

---
+++ apps/coursera/src/app/(pages)/search/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { TogglePageButton } from "@/components/togglePageButton/togglePageButton";
+import { useSearchParams } from "next/navigation";
+import { ReactNode } from "react";
+
+interface landingLayoutParams {
+  children: ReactNode;
+}
+
+const landingLayout = (params: landingLayoutParams) => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const pageNo = Number(searchParams.get("p"));
+  return (
+    <div>
+      {params.children}
+      <div className="flex flex-row items-center justify-center mt-6">
+        <TogglePageButton query={query as string} pageNo={pageNo} />
+      </div>
+    </div>
+  );
+};
+
+export default landingLayout;
