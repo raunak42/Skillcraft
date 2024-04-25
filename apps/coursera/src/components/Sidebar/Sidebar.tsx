81fb06a52b3a7bc97d193049a9386c16333f6f6d
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-04-25 23:52:59 +0530

    Categories added in sidebar, blocked scroll when siedebar is open.

--- apps/coursera/src/components/Sidebar/Sidebar.tsx
+++ apps/coursera/src/components/Sidebar/Sidebar.tsx
@@ -2,25 +2,44 @@
 
 import { useRecoilState } from "recoil";
 import { sideBarOpenState } from "state-store";
+import { CategoryCarousel } from "../CategoryCarousel/CategoryCarousel";
+import { useEffect } from "react";
 
 export const Sidebar = () => {
   const [sideBarOpen, setSideBarOpen] = useRecoilState(sideBarOpenState);
 
+  useEffect(() => {
+    if (sideBarOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [sideBarOpen]);
+
   return (
-    <div>
+    <div className="overflow-hidden">
       <div
         className={` transition-all duration-300 ease-in-out ${
-          sideBarOpen ? "md:w-[270px] w-[480px]" : "w-0"
+          sideBarOpen ? "md:w-[270px] w-[340px]" : "w-0"
         }`}
       >
-        <div className="flex flex-row items-center justify-end">
+        <div className="flex flex-row items-center justify-between py-2 px-4">
+          <div className="text-nowrap text-2xl font-semibold">Main menu</div>
           <img
             onClick={() => {
               setSideBarOpen(false);
             }}
             src="cross.svg"
-            className="size-12 hover:cursor-pointer hover:bg-gray-300 rounded-full p-2"
+            className="size-12 hover:cursor-pointer hover:bg-gray-200 rounded-full p-2"
           ></img>
+        </div>
+        <div className=" text-nowrap flex-col">
+          <h3 className=" hover:bg-gray-200 hover:cursor-pointer pl-4 py-2">Login</h3>
+          <h3 className=" hover:bg-gray-200 hover:cursor-pointer pl-4 py-2">Signup</h3>
+          <div>
+          <div className="text-nowrap font-semibold px-4 pt-4">Categories</div>
+            <CategoryCarousel/>
+          </div>
         </div>
       </div>
     </div>
