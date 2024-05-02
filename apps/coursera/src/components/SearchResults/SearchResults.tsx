0844df2621e215dabe543253be6e3d037e7e1149
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-02 18:36:50 +0530

    now searchresults cards take the user to course/[courseId] page; employed useMediaQuery to hide #s at md breakpoint

--- apps/coursera/src/components/SearchResults/SearchResults.tsx
+++ apps/coursera/src/components/SearchResults/SearchResults.tsx
@@ -1,19 +1,19 @@
 import { PrismaCourseOutput } from "types";
+import { useMediaQuery } from "react-responsive";
 
 interface SRProps {
-  course: PrismaCourseOutput<{ select: {}; include: {} }>;
+  course: PrismaCourseOutput<{ select: {}; include: { admin: true } }>;
 }
 
-
 const handleClickLink = (query: string) => {
-  window.location.assign(
-    `/search?q=${encodeURIComponent(query)}&p=${1}`
-  );
+  window.location.assign(`/search?q=${encodeURIComponent(query)}&p=${1}`);
 };
 
 const handleClick = () => {};
 
 export const SearchResults: React.FC<SRProps> = ({ course }: SRProps) => {
+  const isMdScreen = useMediaQuery({ minWidth: 768 }); //md breakpoint
+
   if (!Array.isArray(course.category)) {
     return;
   }
@@ -22,10 +22,14 @@ export const SearchResults: React.FC<SRProps> = ({ course }: SRProps) => {
     categoryString = categoryString + " " + "#" + course.category[i];
   }
   return (
-    <div className="group h-[270px] sm:h-[140px] lg:w-full lg:h-[200px] xl:h-[260px] pb-1 sm:pb-0 flex flex-col w-full sm:flex-row  bg-white border-[1.5px] border-black sm:border-none rounded-xl overflow-hidden">
+    <div 
+    onClick={()=>[
+      window.location.assign(`/course/${course.id}`)
+    ]}
+    className="group h-[115px] sm:h-[140px] lg:w-full lg:h-[200px] xl:h-[260px] flex w-full flex-row  bg-white  sm:border-none rounded-xl overflow-hidden">
       <div
         onClick={handleClick}
-        className="border-b-[1.5px] border-b-black sm:border-0 h-[80%] sm:h-full sm:w-[30%] lg:w-[40%] xl:w-[45%]  hover:cursor-pointer overflow-hidden flex flex-row items-center justify-center"
+        className="border-0 h-full w-[35%] sm:w-[30%] lg:w-[40%] xl:w-[45%]  hover:cursor-pointer overflow-hidden flex flex-row items-center justify-center"
       >
         <div className="relative h-full w-full  lg:rounded-xl  overflow-hidden">
           <img
@@ -40,7 +44,7 @@ export const SearchResults: React.FC<SRProps> = ({ course }: SRProps) => {
       </div>
       <div
         onClick={handleClick}
-        className="mt-2 pl-2 hover:cursor-pointer lg:ml-12  lg:w-[70%] sm:w-[70%] flex flex-col justify-between lg:py-2"
+        className="mt-2 pl-2 hover:cursor-pointer lg:ml-12 w-[65%] lg:w-[70%] sm:w-[70%] flex flex-col  lg:py-2"
       >
         <div className="">
           <div className="lg:text-3xl font-bold lg:font-bold">
@@ -49,25 +53,42 @@ export const SearchResults: React.FC<SRProps> = ({ course }: SRProps) => {
           <div className="line-clamp-1 lg:line-clamp-3 lg:text-xl pr-2">
             {course.description}
           </div>
-          <div>
-            {/* <div className="text-xl mb-8 hidden lg:block">View details</div> */}
-            <div className="hidden sm:flex flex-row space-x-2 ">
-              {course.category.map(
+          {/* <div className="text-xl mb-8 hidden lg:block">View details</div> */}
+          <div className="flex flex-row flex-wrap space-x-2">
+            {!isMdScreen &&
+              course.category.slice(0, 2).map(
                 (tag, index) =>
                   tag !== "" && (
                     <div
                       key={index}
-                      className="w-fit text-sm  text-[#007bff] hover:cursor-pointer"
+                      className="text-sm  text-[#007bff] hover:cursor-pointer"
                       onClick={() => handleClickLink(tag)}
                     >
                       {"#" + tag}
                     </div>
                   )
               )}
-            </div>
+            {isMdScreen &&
+              course.category.map(
+                (tag, index) =>
+                  tag !== "" && (
+                    <div
+                      key={index}
+                      className="text-sm  text-[#007bff] hover:cursor-pointer"
+                      onClick={() => handleClickLink(tag)}
+                    >
+                      {"#" + tag}
+                    </div>
+                  )
+              )}
           </div>
         </div>
-        <div className="hidden sm:flex flex-row justify-between pr-4">
+        <div>
+          <h1 className="hidden sm:block font-semibold text-xs md:text-sm lg:text-lg xl:text-xl xl:pt-4">
+            By {course.admin?.username}
+          </h1>
+        </div>
+        <div className="flex flex-row justify-between pr-4 mt-auto">
           <div className="flex flex-col justify-end">₹{course.price}/-</div>
           <div>
             <div className="flex flex-row justify-end">
