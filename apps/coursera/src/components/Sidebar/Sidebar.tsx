5e5685395922f578a54fe33df8dd47a884c46f21
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-02 18:34:38 +0530

    added login and signup options to sidebar and made it so that the rest of the screen is unscrollable when sidebar is showing

--- apps/coursera/src/components/Sidebar/Sidebar.tsx
+++ apps/coursera/src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 "use client";
-
 import { useRecoilState } from "recoil";
 import { sideBarOpenState } from "state-store";
 import { CategoryCarousel } from "../CategoryCarousel/CategoryCarousel";
 import { useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+import Link from "next/link";
 
-export const Sidebar = () => {
+export const NoSessionSidebar = () => {
   const [sideBarOpen, setSideBarOpen] = useRecoilState(sideBarOpenState);
 
   useEffect(() => {
@@ -16,6 +17,17 @@ export const Sidebar = () => {
     }
   }, [sideBarOpen]);
 
+  const isMdScreen = useMediaQuery({ minWidth: 768 });
+  useEffect(() => {
+    if (isMdScreen) {
+      //Because sidebar hides at md breakpoint
+      document.body.classList.remove("no-scroll");
+    }
+    if (!isMdScreen && sideBarOpen) {
+      document.body.classList.add("no-scroll");
+    }
+  }, [isMdScreen]);
+
   return (
     <div className="overflow-hidden">
       <div
@@ -24,21 +36,36 @@ export const Sidebar = () => {
         }`}
       >
         <div className="flex flex-row items-center justify-between py-2 px-4">
-          <div className="text-nowrap text-2xl font-semibold">Main menu</div>
+          <div className="text-nowrap text-2xl font-semibold">Account</div>
           <img
             onClick={() => {
               setSideBarOpen(false);
             }}
-            src="cross.svg"
+            src="/cross.svg"
             className="size-12 hover:cursor-pointer hover:bg-gray-200 rounded-full p-2"
           ></img>
         </div>
         <div className=" text-nowrap flex-col">
-          <h3 className=" hover:bg-gray-200 hover:cursor-pointer pl-4 py-2">Login</h3>
-          <h3 className=" hover:bg-gray-200 hover:cursor-pointer pl-4 py-2">Signup</h3>
+          <Link
+          onClick={()=>setSideBarOpen(false)}
+           href={"/login"}>
+            <h3 className=" hover:bg-gray-200 hover:cursor-pointer pl-4 py-2">
+              Login
+            </h3>
+          </Link>
+
+          <Link
+          onClick={()=>setSideBarOpen(false)}
+           href={"/signup"}>
+            <h3 className=" hover:bg-gray-200 hover:cursor-pointer pl-4 py-2">
+              Signup
+            </h3>
+          </Link>
           <div>
-          <div className="text-nowrap font-semibold px-4 pt-4">Categories</div>
-            <CategoryCarousel/>
+            <div className="text-nowrap font-semibold px-4 pt-4 pb-2">
+              Categories
+            </div>
+            <CategoryCarousel />
           </div>
         </div>
       </div>
