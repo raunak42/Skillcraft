5bcab4a02e4997c2e40e5e9e1c853f329b07cbab
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-02 18:47:45 +0530

    minor changes

--- apps/coursera/src/app/api/getCourses/route.ts
+++ apps/coursera/src/app/api/getCourses/route.ts
@@ -1,56 +1,18 @@
 import { prisma } from "@/lib/prisma";
-// import fs from "fs"
-// import path from "path";
-// import { courses } from "./courses"
+import { apiResponse, handleApiError } from "helpers";
+// import { allCourses, physicsCourses } from "./courses"
 
-//for ssr use
 export async function GET(req: Request): Promise<Response> {
+    // console.log(1)
     try {
-        const courses = await prisma.course.findMany({});
+        const courses = await prisma.course.findMany({
+            // take:10
+            include: { admin: true }
+        });
 
-
-        // const injectData = () => {
-        //     courses.map(async (course) => {
-        //         await prisma.course.updateMany({
-        //             data: {
-        //                 category: course.category
-        //             }
-        //         });
-        //     });
-        // };
-
-        // const injectData = async () => {
-        //     for (let i = 0; i < courses.length; i++) {
-        //         await prisma.course.update({
-        //             where: {
-        //                 id:courses[i].id
-        //             },
-        //             data:{
-        //                 category:courses[i].category
-        //             }
-        //         })
-        //     }
-        // }
-        // injectData();
-
-        // for (var i = 0; i < 4; i++) {
-        //     injectData();
-        // }
-
-        // const injectData = async()=>{
-        //     await prisma.course.updateMany({
-        //         where:{
-        //             title:"Standard Model of Partice Physics"
-        //         },
-        //         data:{
-        //             title:"Standard Model of Particle Physics"
-        //         }
-        //     })
-        // }
-        // injectData();
-        return Response.json(courses, { status: 200 })
+        return apiResponse({ data: { courses } }, 200);
     } catch (error) {
-        console.error(error)
-        return new Response("internal server error", { status: 500 })
+        return handleApiError(error)
     }
-}   
+}
+
