d43104867756ee155885cd393d6a6e888ca56940
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-02 18:47:00 +0530

    modified to include admin in the query

--- apps/coursera/src/app/api/getTopCourses/route.ts
+++ apps/coursera/src/app/api/getTopCourses/route.ts
@@ -6,10 +6,10 @@ import { PrismaCourseOutput } from "types";
 export async function GET(req: Request): Promise<Response> {
     try {
 
-        const cachedTopCourses: PrismaCourseOutput<{ select: {}, include: {} }>[] = JSON.parse(await redis.get("topCourses") as string)
-        if (cachedTopCourses) {
-            return apiResponse({ data: { courses: cachedTopCourses } },200)
-        }
+        // const cachedTopCourses: PrismaCourseOutput<{ select: {}, include: {} }>[] = JSON.parse(await redis.get("topCourses") as string)
+        // if (cachedTopCourses) {
+        //     return apiResponse({ data: { courses: cachedTopCourses } },200)
+        // }
         const topCourses = await prisma.course.findMany({
             orderBy: {
                 users: {
@@ -19,6 +19,7 @@ export async function GET(req: Request): Promise<Response> {
             take: 10,
             include: {
                 users: true,
+                admin:true
             },
             distinct: "title"
         });
