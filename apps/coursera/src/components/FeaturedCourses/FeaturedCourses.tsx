81f9b2f104a78bd2e0a54d7984fd9fa90212fb73
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-18 09:57:16 +0530

    moved the tsx into a seperate component

--- apps/coursera/src/components/FeaturedCourses/FeaturedCourses.tsx
+++ apps/coursera/src/components/FeaturedCourses/FeaturedCourses.tsx
@@ -1,6 +1,8 @@
 import { BASE_URL_DEV } from "@/lib/constants";
 import { ApiResponseAttributes, featuredCourses } from "types";
 import { CourseCard } from "../Coursecard/CourseCard";
+import { ViewAllButton } from "./ViewAllButton";
+import { SmallCarousel } from "./SmallCarousel";
 
 export const FeaturedCourses = async () => {
   const res = await fetch(`${BASE_URL_DEV}/api/getFeaturedCourses`, {
@@ -26,28 +28,11 @@ export const FeaturedCourses = async () => {
               </div>
             </div>
             <button className="flex flex-row items-center justify-between hover:bg-gray-200 rounded-lg ">
-              <div className=" font-semibold ">View all</div>
-              <img src="arrowRight.svg" className="w-8"></img>
+              <ViewAllButton query={object.category as string} />
             </button>
           </div>
 
-          <div className="flex flex-row overflow-x-auto no-scrollbar space-x-4 mt-2">
-            {object.courses.map((course) => (
-              <div
-                key={course.id}
-              >
-                <CourseCard
-                  key={course.id}
-                  title={course.title}
-                  imageLink={course.imageLink as string}
-                  description={course.description as string}
-                  price={course.price}
-                  adminUsername={course.admin?.username as string}
-                  courseId={course.id as number}
-                />
-              </div>
-            ))}
-          </div>
+          <SmallCarousel object={object}/>
         </div>
       ))}
     </div>
