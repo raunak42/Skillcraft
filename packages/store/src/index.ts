9cda17239d48849b7efefcc050d24416b06bcc4e
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-18 09:45:11 +0530

    atoms exported

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,3 +1,6 @@
 export  * from "./atoms/sideBarOpen";
 export * from "./atoms/showDropDown"
+export * from "./atoms/avatar"
+export * from "./atoms/username"
+export * from "./atoms/courseClicked"
 
