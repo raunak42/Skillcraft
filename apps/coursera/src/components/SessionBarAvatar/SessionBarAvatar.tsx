1df13ed3bccca2af0492096fcfb6d6dba625108d
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-18 09:47:12 +0530

    component for avatar in session navbar

---
+++ apps/coursera/src/components/SessionBarAvatar/SessionBarAvatar.tsx
@@ -0,0 +1,35 @@
+'use server'
+import { validateRequest } from "@/auth";
+import { BASE_URL_DEV } from "@/lib/constants";
+import { ApiResponseAttributes } from "types";
+
+export const SessionBarAvatar = async () => {
+  const { session } = await validateRequest();
+
+  const res = await fetch(`${BASE_URL_DEV}/api/me`, {
+    cache: "no-store",
+    method: "POST",
+    body: JSON.stringify({
+      session,
+    }),
+  });
+
+  const response: ApiResponseAttributes = await res.json();
+  const user = response.data?.user;
+  const avatar = user?.avatar as string;
+  const username = user?.username as string;
+
+  return (
+    <div
+      className={`${!response && "animated-gradient"}  overflow-hidden shrink-0  size-8 lg:size-[36px] rounded-full  flex flex-row items-center justify-center`}
+    >
+      {response && <img className="size-full" src={avatar as string}></img>}
+      {!response && (
+        <div
+          className="size-full bg-gray-300 rounded-full"
+          // src="/emptyAvatar.svg"
+        ></div>
+      )}
+    </div>
+  );
+};
