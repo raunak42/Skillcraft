56ed61e465dad7172b2618b3a51efa07b555405a
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-18 09:50:23 +0530

    needed session info in sessionNavbar but validateRequest() can only be called from server components, hence the component.

---
+++ apps/coursera/src/components/navbar/SessionNavbarSSR.tsx
@@ -0,0 +1,7 @@
+import { validateRequest } from "@/auth";
+import SessionNavbar from "./SessionNavbar";
+
+export const SessionNavbarSSR: React.FC = async () => {
+  const {session, user} = await validateRequest();
+  return <SessionNavbar session={session} authUser={user} />;
+};
