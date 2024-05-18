197de295101371ed23c55506090f2a4efa27a961
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-18 09:54:39 +0530

    added loading spinners to the buttons

--- apps/coursera/src/components/Login/Login.tsx
+++ apps/coursera/src/components/Login/Login.tsx
@@ -1,17 +1,22 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-interface LoginProps{
-    buttonText:string
+import Link from "next/link";
+import { useState } from "react";
+interface LoginProps {
+  buttonText: string;
 }
-export const Login:React.FC<LoginProps> = ({buttonText}:LoginProps)=>{
-    return<div>
-    <div className="w-[330px] h-[410px] sm:w-[440px] sm:h-[360px] border-[1.5px] border-black rounded-xl bg-white flex flex-col gap-8 sm:gap-8">
+export const Login: React.FC<LoginProps> = ({ buttonText }: LoginProps) => {
+  const [githubClicked, setGithubClicked] = useState<boolean>(false);
+  const [googleClicked, setGoogleClicked] = useState<boolean>(false);
+
+  return (
+    <div>
+      <div className="w-[330px] h-[410px] sm:w-[440px] sm:h-[360px] border-[1.5px] border-black rounded-xl bg-white flex flex-col gap-8 sm:gap-8">
         <div className="flex flex-col items-center w-full pt-4 gap-2">
           <div className="w-[90%]">
             <h1 className="">Username</h1>
             <input
-            autoFocus={true}
+              autoFocus={true}
               name="username"
               id="username"
               placeholder=""
@@ -23,33 +28,64 @@ export const Login:React.FC<LoginProps> = ({buttonText}:LoginProps)=>{
             <input
               name="password"
               id="password"
+              type="password"
               placeholder=""
               className="bg-gray-100 w-full h-[40px] rounded-full px-4"
             ></input>
           </div>
 
-          <button
-            className="rounded-full bg-black text-white font-semibold px-12 py-2 mt-4"
-          >
+          <button className="rounded-full bg-black text-white font-semibold px-12 py-2 mt-4">
             {buttonText}
           </button>
         </div>
 
-      <div className="flex flex-col items-center justify-center gap-2">
-        <h1 className="font-semibold">Or</h1>
-        <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
-          <Link href={"/login/google"} className="hover:bg-gray-200 rounded-full">
-            <img src="googleSignin.svg" className=""></img>
-          </Link>
-          <Link
-            href={"/login/github"}
-            className="px-[12px] py-[10px] border-[1px] border-gray-500 w-[189px] h-[40px] rounded-full flex flex-row items-center justify-between hover:bg-gray-200"
-          >
-            <img className="size-[20px]" src="github.svg"></img>
-            <h1 className="roboto-medium">Continue with GitHub</h1>
-          </Link>
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h1 className="font-semibold">Or</h1>
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+            {!googleClicked && (
+              <Link
+              onClick={()=>{
+                setGoogleClicked(true)
+              }}
+                href={"/login/google"}
+                className="hover:bg-gray-200 rounded-full"
+              >
+                <img src="/googleSignin.svg" className=""></img>
+              </Link>
+            )}
+            {googleClicked && (
+              <div className="bg-gray-200 px-[12px] py-[10px] border-[1px] border-gray-500 w-[189px] h-[40px] rounded-full flex flex-col items-center justify-center">
+                <img
+                  className="size-3 animate-spin "
+                  src="/darkSpinner.svg"
+                ></img>
+              </div>
+            )}
+            {!githubClicked && (
+              <Link
+              onClick={()=>{
+                setGithubClicked(true)
+              }}
+                href={"/login/github"}
+                className="px-[12px] py-[10px] border-[1px] border-gray-500 w-[189px] h-[40px] rounded-full flex flex-row items-center justify-between hover:bg-gray-200"
+              >
+                <img className="size-[20px]" src="/github.svg"></img>
+                <h1 className="roboto-medium">Continue with GitHub</h1>
+              </Link>
+            )}
+            {githubClicked && (
+              <div
+                className="bg-gray-200 px-[12px] py-[10px] border-[1px] border-gray-500 w-[189px] h-[40px] rounded-full flex flex-col items-center justify-center"
+              >
+                <img
+                  className="size-3 animate-spin "
+                  src="/darkSpinner.svg"
+                ></img>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-}
+  );
+};
