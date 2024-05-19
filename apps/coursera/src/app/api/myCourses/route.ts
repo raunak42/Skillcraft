112d35aea3c3e7c8d57ee9000087187ad90782e0
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-19 23:08:22 +0530

    Some changes.

--- apps/coursera/src/app/api/myCourses/route.ts
+++ apps/coursera/src/app/api/myCourses/route.ts
@@ -2,26 +2,31 @@ import { ADMIN_NOT_FOUND_MESSAGE, SESSION_HEADER_MISSING_MESSAGE } from "@/lib/c
 import { prisma } from "@/lib/prisma";
 import { apiResponse, getSessionDataFromMiddleware, handleApiError } from "helpers";
 
-export async function GET(req: Request): Promise<Response> {
+export async function POST(req: Request): Promise<Response> {
     try {
+        console.log("myCourses route")
         const sessionData = getSessionDataFromMiddleware(req)
         if (!sessionData) {
             return apiResponse({ message: SESSION_HEADER_MISSING_MESSAGE }, 401);//500 internal server error because middleware not working
         }
-        const adminId = sessionData.session.userId;
+        const userId = sessionData.session.userId;
 
-        const admininDb = await prisma.admin.findUnique({
+        const userinDb = await prisma.user.findUnique({
             where: {
-                id: adminId
+                id: userId
             },
             include: {
-                createdCourses: true
+                courses: {
+                    include: {
+                        admin: true
+                    }
+                },
             }
         });
-        if (!admininDb) {
-            return apiResponse({ message: ADMIN_NOT_FOUND_MESSAGE}, 404)
+        if (!userinDb) {
+            return apiResponse({ message: ADMIN_NOT_FOUND_MESSAGE }, 404)
         }
-        const myCourses = admininDb.createdCourses;
+        const myCourses = userinDb.courses;
         return apiResponse({ data: { courses: myCourses } }, 200)
     } catch (error) {
         return handleApiError(error)
