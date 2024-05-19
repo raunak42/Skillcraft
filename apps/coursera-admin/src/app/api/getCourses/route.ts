df5374d1ead34b9e8ea3528f36722d0ff15298a0
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-19 23:02:29 +0530

    script for injecting video links

--- apps/coursera-admin/src/app/api/getCourses/route.ts
+++ apps/coursera-admin/src/app/api/getCourses/route.ts
@@ -4,6 +4,7 @@ import { allCourses, physicsCourses } from "./courses"
 
 export async function GET(req: Request): Promise<Response> {
     try {
+        console.log("hello")
         const courses = await prisma.course.findMany({});
 
         // const totalCourses = physicsCourses.concat(allCourses)
@@ -46,6 +47,38 @@ export async function GET(req: Request): Promise<Response> {
         // };
 
         // injectData2()
+
+        // const injectChapterVideoLinks = async()=>{
+        //     await prisma.course.updateMany({
+        //         where:{
+        //             chapterVideoLinks:{
+        //                 hasEvery:[]
+        //             }
+        //         },
+        //         data:{
+        //            chapterVideoLinks:[
+        //             "https://videos.pexels.com/video-files/15921892/15921892-sd_640_360_25fps.mp4",
+        //             "https://videos.pexels.com/video-files/16666848/16666848-sd_640_360_24fps.mp4",
+        //             "https://videos.pexels.com/video-files/9712579/9712579-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/6561360/6561360-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/20317587/20317587-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/13795766/13795766-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/20801280/20801280-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/20792398/20792398-sd_640_360_25fps.mp4",
+        //             "https://videos.pexels.com/video-files/20788616/20788616-sd_640_360_25fps.mp4",
+        //             "https://videos.pexels.com/video-files/20771551/20771551-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/19757074/19757074-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/20315562/20315562-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/20053504/20053504-sd_640_360_25fps.mp4",
+        //             "https://videos.pexels.com/video-files/20179799/20179799-sd_540_960_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/20241376/20241376-sd_640_360_30fps.mp4",
+        //             "https://videos.pexels.com/video-files/10028146/10028146-sd_640_360_24fps.mp4"
+        //            ]
+        //         }
+        //     })
+        // }
+
+        // await injectChapterVideoLinks()
 
 
         return apiResponse({ data: { courses } }, 200);
