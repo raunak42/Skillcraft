385149c08c5b1a48b8465ca03db64bd846a4386d
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-19 23:06:32 +0530

    Payment component for cart.

---
+++ apps/coursera/src/components/CartPayment/CartPayment.tsx
@@ -0,0 +1,36 @@
+import { BASE_URL_DEV } from "@/lib/constants";
+import { TheForm } from "../TheForm/TheForm";
+import { validateRequest } from "@/auth";
+import { PrismaCourseOutput } from "types";
+
+interface CartPaymentProps {
+  courses: PrismaCourseOutput<{
+    select: {};
+    include: { admin: true; users: true };
+  }>[];
+}
+
+export const CartPayment: React.FC<CartPaymentProps> = async ({ courses }) => {
+  const { session, user } = await validateRequest();
+
+  const res = await fetch(`${BASE_URL_DEV}/api/cartCheckoutSession`, {
+    method: "POST",
+    cache: "no-store",
+    body: JSON.stringify({
+      courses,
+      authSession: session,
+      user,
+    }),
+  });
+  const response = await res.json();
+
+  if (!response) {
+    return <div>Loading...</div>;
+  }
+  const clientSecret = response.clientSecret;
+  return (
+    <div className="" id="checkout">
+      {response && clientSecret && <TheForm clientSecret={clientSecret} />}
+    </div>
+  );
+};
