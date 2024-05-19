2f021498fb1a147948a7338a882fbdb2971e4857
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-19 23:05:27 +0530

    Changes for better loading ux

--- apps/coursera/src/components/FeaturedCourses/ViewAllButton.tsx
+++ apps/coursera/src/components/FeaturedCourses/ViewAllButton.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import { useRecoilState } from "recoil";
+import { courseClickedState } from "state-store";
+
 interface ViewAllButtonProps {
   query: string;
 }
 
 export const ViewAllButton: React.FC<ViewAllButtonProps> = ({ query }) => {
+  const [courseClicked, setCourseClicked] = useRecoilState(courseClickedState)
   const firstPage = 1;
   return (
     <div
       className="flex flex-row items-center"
       onClick={() => {
+        setCourseClicked(true)
         window.location.assign(
           `/search?q=${encodeURIComponent(query)}&p=${firstPage}`
         );
