a9dbdec6b8b6944ed19b7832169305e3562c4181
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-21 21:53:33 +0530

    Component to be used on wishlist page.

---
+++ apps/coursera/src/app/(pages)/wishlist/ListItems.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { CourseCard } from "@/components/Coursecard/CourseCard";
+import { Session, User } from "lucia";
+import { useState } from "react";
+import { ApiResponseAttributes, PrismaCourseOutput } from "types";
+
+interface ListItemsProps {
+  course: PrismaCourseOutput<{ select: {}; include: { admin: true } }>;
+  session: Session | null;
+  user: User | null;
+}
+
+export const ListItems: React.FC<ListItemsProps> = ({
+  course,
+  session,
+  user,
+}) => {
+  const [removeClicked, setRemoveClicked] = useState<boolean>(false);
+  const [response, setResponse] = useState<ApiResponseAttributes>();
+
+  const RemoveItem = async (courseId: number) => {
+    const res = await fetch(`/api/removeFromWishlist`, {
+      method: "POST",
+      body: JSON.stringify({
+        courseIdToRemove: courseId,
+        data: { session, user },
+      }),
+    });
+
+    const response = await res.json();
+    setResponse(response);
+    // window.location.reload();
+  };
+
+  return (
+    !removeClicked && (
+      <div className="ml-4 mt-6">
+        <CourseCard
+          imageLink={course.imageLink as string}
+          title={course.title}
+          description={course.description as string}
+          adminUsername={course.admin?.username as string}
+          key={course.id}
+          price={course.price}
+          courseId={course.id as number}
+        />
+        <div className="mt-2 flex flex-row justify-end rounded-full overflow-hidden">
+          <img
+            onClick={() => {
+              setRemoveClicked(true);
+              RemoveItem(course.id!);
+            }}
+            className="size-4 hover:cursor-pointer rounded-full hover:bg-white"
+            src="/minus.svg"
+          ></img>
+        </div>
+      </div>
+    )
+  );
+};
