afaf49403ede1b5c0750cdbd5fc54aa2b28fb2be
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-21 21:50:53 +0530

    New route createed to remove an item from wishlist.

---
+++ apps/coursera/src/app/api/removeFromWishlist/route.ts
@@ -0,0 +1,49 @@
+import { ADMIN_NOT_FOUND_MESSAGE, SESSION_HEADER_MISSING_MESSAGE } from "@/lib/constants";
+import { prisma } from "@/lib/prisma";
+import { apiResponse, getSessionDataFromMiddleware, handleApiError } from "helpers";
+
+export async function POST(req: Request): Promise<Response> {
+    try {
+        const body = await req.json();
+        const courseIdToRemove = body.courseIdToRemove
+        // console.log(body)
+        const sessionData = getSessionDataFromMiddleware(req)
+        if (!sessionData) {
+            return apiResponse({ message: SESSION_HEADER_MISSING_MESSAGE }, 401);//500 internal server error because middleware not working
+        }
+        const userId = sessionData.session.userId;
+
+        const userinDb = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                // cart: true,
+                courses: true,
+                wishList:true
+            }
+        });
+
+        const wishlist = userinDb?.wishList!
+        const updatedWishlist = wishlist.filter(courseId => courseId !== courseIdToRemove);
+
+
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                wishList: updatedWishlist
+            }
+        })
+        console.log("Successfully removed.")
+
+        if (!userinDb) {
+            return apiResponse({ message: ADMIN_NOT_FOUND_MESSAGE }, 404)
+        }
+        const myCourses = userinDb.courses;
+        return apiResponse({ data: { courses: myCourses } }, 200)
+    } catch (error) {
+        return handleApiError(error)
+    }
+}
