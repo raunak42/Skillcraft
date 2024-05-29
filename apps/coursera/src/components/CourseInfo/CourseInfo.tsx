20b26ce0e14a143b2360dbce17762e39278538de
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-29 18:56:32 +0530

    Style changes.

--- apps/coursera/src/components/CourseInfo/CourseInfo.tsx
+++ apps/coursera/src/components/CourseInfo/CourseInfo.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 import { validateRequest } from "@/auth";
 import { Login } from "../Login/Login";
 import { Buttons } from "./Buttons";
-import { handleLogin } from "@/app/(pages)/login/page";
+import { handleLogin } from "@/app/(pages)/login/[status]/page";
 
 interface CourseInfoProps {
   imageLink: string;
@@ -31,51 +31,41 @@ export const CourseInfo: React.FC<CourseInfoProps> = async ({
   return (
     <div className=" grid grid-cols-7 h-full bg-[#ffffff] mx-[15px] rounded-2xl lg:gap-4 xl:gap-0 xl:mx-[50px] overflow-hidden ">
       <div
-        className={`py-2 px-4 col-span-7 ${session && "md:col-span-4"} lg:col-span-4 md:py-4 px-2 flex flex-col  gap-10`}
+        className={`py-2 px-4 col-span-7 ${session && "md:col-span-4"} ${session && "lg:col-span-4"} ${!session && "lg:col-span-7"} md:py-4 px-2 flex flex-col  gap-10`}
       >
-        <div className=" flex flex-col items-start justify-start">
-          <div className=" flex flex-col  items-start justify-start">
-            <h1 className="text-3xl lg:text-4xl xl:text-4xl font-bold md:pt-0">
-              {title}
-            </h1>
-            <h1 className=" text-md lg:text-xl xl:text-lg ">{description}</h1>
-            <h1 className="text-xs lg:text-sm flex flex-row gap-1 pt-4">
-              Created by
-              <p className="font-semibold"> {adminUsername}</p>
-            </h1>
-          </div>
-
-          <div className="w-full">
-            <div className="relative flex flex-row items-center justify-center shrink-0 w-full">
-              <div className="rounded-xl absolute inset-0 bg-gradient-radial from-transparent to-black/40"></div>
-              <img
-                className="rounded-xl shrink-0 size-full"
-                src={imageLink as string}
-                alt={title as string}
-              ></img>
-              <h1 className="absolute left-2 bottom-2 text-white flex flex-row justify-start ml-4 md:ml-0 mt-4 w-full text-2xl md:text-2xl font-bold">
-                ₹{price}/-
+        <div
+          className={` flex ${!session && "grid grid-cols-5"} ${session && "flex-col"} items-start justify-start`}
+        >
+          <div className="flex flex-col col-span-5 md:col-span-3">
+            <div className=" flex flex-col  items-start justify-start">
+              <h1 className="text-3xl lg:text-4xl xl:text-4xl font-bold md:pt-0">
+                {title}
+              </h1>
+              <h1 className=" text-md lg:text-xl xl:text-lg ">{description}</h1>
+              <h1 className="text-xs lg:text-sm flex flex-row gap-1 pt-4">
+                Created by
+                <p className="font-semibold"> {adminUsername}</p>
               </h1>
             </div>
-
-            {/* <div className="px-2 md:px-0 w-full flex flex-row items-center justify-between gap-2">
-              <button
-                // onClick={handleBuy}
-                className="   bg-black mt-4 px-6 py-2 lg:mt-6  text-sm lg:text-md font-semibold  text-white"
-              >
-                Add to wishlist
-              </button>
-              <button
-                onClick={addToCart}
-                className="   bg-black mt-4 px-6 py-2 lg:mt-6  text-sm lg:text-md font-semibold  text-white"
-              >
-                + Add to cart
-              </button>
-            </div> */}
-            <Buttons session={session} user={user} courseId={courseId} />
+            <div className="w-full">
+              <div className="relative flex flex-row items-center justify-center shrink-0 w-full">
+                <div className="rounded-xl absolute inset-0 bg-gradient-radial from-transparent to-black/40"></div>
+                <img
+                  className="rounded-xl shrink-0 size-full"
+                  src={imageLink as string}
+                  alt={title as string}
+                ></img>
+                <h1 className="absolute left-2 bottom-2 text-white flex flex-row justify-start ml-4 md:ml-0 mt-4 w-full text-2xl md:text-2xl font-bold">
+                  ₹{price}/-
+                </h1>
+              </div>
+              <Buttons session={session} user={user} courseId={courseId} />
+            </div>
           </div>
 
-          <div className="pb-4 md:pb-0 pt-10">
+          <div
+            className={`pb-4 md:pb-0 pt-10 col-span-5 md:col-span-2 ${!session && "flex flex-col items-start md:items-center justify-center"}`}
+          >
             <h1 className=" lg:text-2xl xl:text-2xl font-semibold">
               What you'll learn :-
             </h1>
@@ -85,7 +75,7 @@ export const CourseInfo: React.FC<CourseInfoProps> = async ({
                 {chapters?.map((chapter, index) => (
                   <h1
                     key={index}
-                    className="lg:text-lg xl:text-base text-sm lg:pl-[18px] pl-[10px]"
+                    className="lg:text-base xl:text-lg text-sm lg:pl-[18px] pl-[10px]"
                   >
                     • Chapter {index + 1}: {chapter}
                     <br />
@@ -94,6 +84,11 @@ export const CourseInfo: React.FC<CourseInfoProps> = async ({
                 ))}
               </div>
             </div>
+            {!session && (
+              <a href="/login/fresh" className="hover:cursor-pointer w-[240px] lg:w-[300px] h-[46px] flex items-center justify-center text-white font-semibold  rounded-full bg-green-500 mt-[30px]">
+                Login to purchase
+              </a>
+            )}
           </div>
         </div>
       </div>
@@ -120,7 +115,7 @@ export const CourseInfo: React.FC<CourseInfoProps> = async ({
           </div>
         </Suspense>
       )}
-      {!session && (
+      {/* {!session && (
         <div className=" bg-gray-300/90 shadow-inner  col-span-7 md:px-4  xl:col-span-3 md:py-4  flex flex-col items-center justify-center xl:justify-start gap-8 py-4 ">
           <div className=" flex flex-row items-center justify-center gap-4">
             <img className="size-8 lg:size-10" src="/login.svg"></img>
@@ -133,7 +128,7 @@ export const CourseInfo: React.FC<CourseInfoProps> = async ({
               action={handleLogin}
               className="flex flex-col items-center justify-center"
             >
-              <Login buttonText="Login" />
+              <Login session={session} buttonText="Login" />
             </form>
             <div className="flex flex-row">
               Or{""}
@@ -141,7 +136,7 @@ export const CourseInfo: React.FC<CourseInfoProps> = async ({
             </div>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
