1031a252a6ffd6e6e943aca49ee88a8fbcfaf14a
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-29 18:29:01 +0530

    Avatar component for navbar of admin app.

---
+++ apps/coursera-admin/src/components/navbar/AvatarComponent.tsx
@@ -0,0 +1,35 @@
+import { BASE_URL_DEV } from "@/lib/constants";
+import { ApiResponseAttributes } from "types";
+
+interface AvatarComponentProps {}
+export const AvatarComponent: React.FC<AvatarComponentProps> = async () => {
+  const fetchData = async () => {
+    const res = await fetch(`${BASE_URL_DEV}/api/me`, {
+      method: "POST",
+      cache: "no-store",
+    });
+
+    const response: ApiResponseAttributes = await res.json();
+    return response;
+  };
+
+  const response = await fetchData();
+
+  const user = response.data?.user;
+  const username = user?.username;
+  const avatar = user?.avatar;
+
+  return (
+    <div
+      className={`${!response && "animated-gradient"}  overflow-hidden shrink-0  size-8 lg:size-[36px] rounded-full  flex flex-row items-center justify-center`}
+    >
+      {response && <img className="size-full" src={avatar as string}></img>}
+      {!response && (
+        <div
+          className="size-full bg-gray-300 rounded-full"
+          // src="/emptyAvatar.svg"
+        ></div>
+      )}
+    </div>
+  );
+};
