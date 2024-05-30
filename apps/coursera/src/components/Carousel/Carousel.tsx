c2da9540dfd938adc3abe851ebb83492e890b5ba
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-05-30 23:15:18 +0530

    Added scroll gesture suggestion and stopped calling api route for courses.

--- apps/coursera/src/components/Carousel/Carousel.tsx
+++ apps/coursera/src/components/Carousel/Carousel.tsx
@@ -1,37 +1,45 @@
-import React from "react";
-import { ApiResponseAttributes } from "types";
-import { BASE_URL_DEV } from "@/lib/constants";
+"use client";
+import React, { useState } from "react";
+import { PrismaCourseOutput } from "types";
 import { CarouselCard } from "../CarouselCard/CarouselCard";
 
-export const Carousel: React.FC = async () => {
-  const res = await fetch(`${BASE_URL_DEV}/api/getTopCourses`, {
-    cache: "default",
-  });
-  const response: ApiResponseAttributes = await res.json();
+interface CarouselProps {
+  courses: PrismaCourseOutput<{ select: {}; include: {} }>[];
+}
 
-  const topCourses = response.data?.courses;
-  if (!topCourses) {
-    return <div>No courses available.</div>;
-  }
+export const Carousel: React.FC<CarouselProps> = ({ courses }) => {
+  const [showAnimation, setShowAnimation] = useState<boolean>(true);
+
+  setTimeout(() => {
+    setShowAnimation(false)
+  }, 2000);
 
   return (
     <div className="relative w-full h-full border-2 lg:px-2 rounded-xl ">
-      <div className="w-full h-full rounded-xl overflow-x-auto snap-x snap-mandatory flex flex-col items-center justify-center">
-        <div className="flex w-[100%] h-[99%] hover:cursor-pointer space-x-4 ">
-          {topCourses.map((course,index) => {
+      <div className=" relative w-full h-full rounded-xl overflow-x-auto snap-x snap-mandatory flex flex-col items-center justify-center">
+        <div
+          className={`flex w-[100%] h-[99%] hover:cursor-pointer space-x-4  `}
+        >
+          {courses.map((course, index) => {
             return <CarouselCard course={course} key={index} />;
           })}
         </div>
+        {showAnimation&&<div className="bg-gradient-radial from-transparent to-black rounded-xl w-[84px] h-[60px] md:w-56 md:h-40 flex flex-col items-center justify-center absolute">
+          <img
+            src="/scroll.svg"
+            className="md:size-20 size-8 animate-bounce mr-[5%]"
+          ></img>
+        </div>}
       </div>
 
       {/*arrows*/}
-      <div className="absolute bottom-[46%]">
+      <div className="sm:hidden absolute bottom-[45%]">
         <img
           src="arrowPrev.svg"
           className=" size-6 sm:size-9 xl:size-11 hover:cursor-default"
         ></img>
       </div>
-      <div className="absolute bottom-[45%] right-[0%] animate-bounce">
+      <div className="sm:hidden absolute bottom-[45%] right-[1%]">
         <img
           src="arrowNext.svg"
           className=" size-6 sm:size-9 xl:size-11 hover:cursor-default"
