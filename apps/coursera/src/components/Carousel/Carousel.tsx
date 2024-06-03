759d1f698052bd634f351ca87113aa6639a13e45
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-03 22:56:54 +0530

    Minor changes.

--- apps/coursera/src/components/Carousel/Carousel.tsx
+++ apps/coursera/src/components/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PrismaCourseOutput } from "types";
 import { CarouselCard } from "../CarouselCard/CarouselCard";
 
@@ -10,9 +10,11 @@ interface CarouselProps {
 export const Carousel: React.FC<CarouselProps> = ({ courses }) => {
   const [showAnimation, setShowAnimation] = useState<boolean>(true);
 
-  setTimeout(() => {
-    setShowAnimation(false)
-  }, 2000);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAnimation(false);
+    }, 2000);
+  }, []);
 
   return (
     <div className="relative w-full h-full border-2 lg:px-2 rounded-xl ">
@@ -24,12 +26,14 @@ export const Carousel: React.FC<CarouselProps> = ({ courses }) => {
             return <CarouselCard course={course} key={index} />;
           })}
         </div>
-        {showAnimation&&<div className="bg-gradient-radial from-transparent to-black rounded-xl w-[84px] h-[60px] md:w-56 md:h-40 flex flex-col items-center justify-center absolute">
-          <img
-            src="/scroll.svg"
-            className="md:size-20 size-8 animate-bounce mr-[5%]"
-          ></img>
-        </div>}
+        {showAnimation && (
+          <div className="bg-gradient-radial from-transparent to-black rounded-xl w-[84px] h-[60px] md:w-56 md:h-40 flex flex-col items-center justify-center absolute">
+            <img
+              src="/scroll.svg"
+              className="md:size-20 size-8 animate-bounce mr-[5%]"
+            ></img>
+          </div>
+        )}
       </div>
 
       {/*arrows*/}
