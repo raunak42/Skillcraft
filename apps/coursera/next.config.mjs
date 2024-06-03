72b1b028637caa9f50ff250367d99fed48c50a24
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-03 23:02:32 +0530

    Configured to allow images from all domains.

--- apps/coursera/next.config.mjs
+++ apps/coursera/next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   // experimental:{
   //   serverActions:true
   // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
   transpilePackages: [
     "components",
     "db",
