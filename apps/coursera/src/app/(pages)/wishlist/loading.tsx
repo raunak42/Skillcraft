f097d412d4db21a5ef2965884bffbbcf9cfb20f7
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-03 22:51:40 +0530

    Similar loading states added for multiple pages.

---
+++ apps/coursera/src/app/(pages)/wishlist/loading.tsx
@@ -0,0 +1,12 @@
+const Loading: React.FC = () => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl font-semibold">Your wishlist:</h1>
+      <div className="flex flex-row flex-wrap  justify-center items-center mb-[300px]">
+        <img src="/spinnerBlack.svg" className="size-20 mt-10 animate-spin"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
