f61227b00974daac1697353e2411489604e9f0cd
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-03 22:52:17 +0530

    Stopped redis.

--- apps/coursera/src/lib/redis.ts
+++ apps/coursera/src/lib/redis.ts
@@ -1,11 +1,11 @@
-import {Redis} from "ioredis";
+// import {Redis} from "ioredis";
 
-const getRedisUrl = () =>{
-    if(process.env.REDIS_URL){
-        return process.env.REDIS_URL
-    }
+// const getRedisUrl = () =>{
+//     if(process.env.REDIS_URL){
+//         return process.env.REDIS_URL
+//     }
 
-    throw new Error("REDIS_URL is not defined")
-}
+//     throw new Error("REDIS_URL is not defined")
+// }
 
-export const redis = new Redis(getRedisUrl());
+// export const redis = new Redis(getRedisUrl());
