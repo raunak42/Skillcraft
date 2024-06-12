b6e2549da71759f1c2cd6aaf4e50aecb5241fa5c
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-12 20:34:03 +0530

    Corrected deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
+cd ~/Skillcraft #The script runs at root of ec2 instance, hence cd into repo first. Yml file says "sudo bash ~/Skillcraft/deploy.sh" but still it says it at the root of the ec2.
 sudo git pull origin main
 yarn install
 yarn build
