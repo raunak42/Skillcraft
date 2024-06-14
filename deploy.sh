ef2f9d087097996c915605dae559c561736cc1d7
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-14 20:15:52 +0530

    Minor script changes.

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/Skillcraft #The script runs at root of ec2 instance, hence cd into repo first. Yml file says "sudo bash ~/Skillcraft/deploy.sh" but still it says it at the root of the ec2. #Writing cd /home/ubuntu/Skillcraft instead of cd ~/Skillcraft because the root dirs of sudo user and normal users are different respectively.
 # git pull origin main
-sudo yarn install
-sudo yarn build
-sudo pm2 kill
+yarn install
+yarn build
+pm2 kill
 bash /home/ubuntu/Skillcraft/stripe_login.sh
-sudo pm2 start yarn --name skillcraft -- start
-sudo pm2 startup | sudo bash #This command generates a startup script and runs it.
-sudo pm2 save                # Save the current PM2 process list for automatic respawn
+pm2 start yarn --name skillcraft -- start
+pm2 startup | bash #This command generates a startup script and runs it.
+pm2 save                # Save the current PM2 process list for automatic respawn
