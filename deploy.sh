f11ac3001830e557ee882cd5343f6338133d1ad5
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-14 12:07:03 +0530

    Prepration for stripe login automation.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
+export $(grep -v '^#' .env | xargs) # Load environment variables from .env file
+
 
 cd /home/ubuntu/Skillcraft #The script runs at root of ec2 instance, hence cd into repo first. Yml file says "sudo bash ~/Skillcraft/deploy.sh" but still it says it at the root of the ec2. #Writing cd /home/ubuntu/Skillcraft instead of cd ~/Skillcraft because the root dirs of sudo user and normal users are different respectively.
 git pull origin main
 yarn install
 yarn build
 sudo pm2 kill
+stripe login --api-key "$STRIPE_SECRET_KEY" 
 sudo pm2 start yarn --name skillcraft -- start
 sudo pm2 startup | sudo bash #This command generates a startup script and runs it.
 sudo pm2 save # Save the current PM2 process list for automatic respawn
