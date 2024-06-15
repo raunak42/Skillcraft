8baddf283a856af210d8d97c9d3f5e3b3b46353c
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-15 20:23:21 +0530

    Minor change.

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 #Keep the user homogeneous, if one command is prefixed with sudo, prefix every other command including how you handle the start of this script in the yml file. Else just do not user sudo anywhere.
 
-cd /home/ubuntu/Skillcraft #The script runs at root of ec2 instance, hence cd into repo first. Yml file says "sudo bash ~/Skillcraft/deploy.sh" but still it says it at the root of the ec2. #Writing cd /home/ubuntu/Skillcraft instead of cd ~/Skillcraft because the root dirs of sudo user and normal users are different respectively.
+cd ~/Skillcraft #The script runs at root of ec2 instance, hence cd into repo first. Yml file says "sudo bash ~/Skillcraft/deploy.sh" but still it says it at the root of the ec2. #Writing cd /home/ubuntu/Skillcraft instead of cd ~/Skillcraft because the root dirs of sudo user and normal users are different respectively.
 # git pull origin main
 yarn install
 yarn cache clean #every time "yarn install" runs, it builds up cache.
 yarn build
 pm2 kill
-bash /home/ubuntu/Skillcraft/stripe_login.sh
+bash ~/Skillcraft/stripe_login.sh
 pm2 start yarn --name skillcraft -- start
 pm2 startup | bash #This command generates a startup script and runs it.
 pm2 save                # Save the current PM2 process list for automatic respawn
