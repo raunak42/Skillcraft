81179423993aff31d1f5e19bad21e0354d1f192c
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-06-23 17:28:17 +0530

    Revert "Trying without server."
    This reverts commit 6f8c13f26d210be9b4405177626902000cde9bf5.

--- apps/skillcraft-user/src/helpers/blurUrlCreator.ts
+++ apps/skillcraft-user/src/helpers/blurUrlCreator.ts
@@ -1,7 +1,7 @@
 import { getPlaiceholder } from "plaiceholder";
 
 export const blurUrlCreator = async (src: string): Promise<string> => {
-    // "use server"
+    "use server"
     const res = await fetch(src)
     const buffer = Buffer.from(await res.arrayBuffer())
     const { base64 } = await getPlaiceholder(buffer);
