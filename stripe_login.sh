19b31cd68d7226079a1195070669f1ae2e3e059a
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-07 22:16:50 +0530

    Pulling the api key from the .env file.

--- stripe_login.sh
+++ stripe_login.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Set your Stripe API key
-API_KEY="sk_test_51PEbJDSFIpXPowBcSg7nFlPBMfRlmcaESDocAp2VEPdQpc6oZotFW0GHfIViyTTyTX8bsQtceUDz20HkO7tHSmvq008ABlr4nH"
-
-# Set the desired device name
+source .env
 DEVICE_NAME="raunak42-server"
 
 # Use the `expect` command to automate the interactive process
 expect <<EOF
 spawn stripe login --interactive
 expect "Enter your API key: "
-send "${API_KEY}\r"
+send "${STRIPE_SECRET_KEY}\r"
 expect "How would you like to identify this device in the Stripe Dashboard?"
 send "${DEVICE_NAME}\r"
 expect eof
